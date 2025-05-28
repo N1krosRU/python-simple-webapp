db28432855c5d802eddbdce06527f9ce5906c39c
Ненахов Артем Александрович <artem.nenakhov@simbirsoft.com>
2025-05-28 11:54:39 +0400

    feat: add clients ip

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import socket
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -13,11 +13,18 @@ APP_MESSAGE = "Hello, World!"
 def hello_world():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
+    # Список IP-адресов из X-Forwarded-For (если есть)
+    forwarded_for = request.headers.getlist("X-Forwarded-For")
+    # Последний прокси (или клиент напрямую)
+    remote_addr = request.remote_addr
+
     return {
         "aplication_message": APP_MESSAGE,
         "aplication_version": APP_VERSION,
         "server_hostname": hostname,
         "server_ip_address": ip_address,
+        "client_ip_forwarded_for": forwarded_for,
+        "client_ip_remote_addr": remote_addr,
         "env": os.environ.get("TEST_ENV", "TEST_ENV not set"),
         "env_secret": os.environ.get("TEST_SECRET_ENV", "TEST_SECRET_ENV not set")
     }
